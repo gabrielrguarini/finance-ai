de6e739019e865d8fde8c8e5be1be6c750849129
Gabriel Guarini <gabrielrguarini@gmail.com>
2024-11-05 20:58:05 -0300

    feat: add authentication with clerk

---
+++ app/login/page.tsx
@@ -0,0 +1,48 @@
+import { Button } from "@/components/ui/button";
+import { SignInButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import { LogInIcon } from "lucide-react";
+import { redirect } from "next/navigation";
+import Image from "next/image";
+
+const LoginPage = async () => {
+  const { userId } = await auth();
+  if (userId) {
+    redirect("/");
+  }
+  return (
+    <div className="grid h-full grid-cols-2">
+      <div className="m-auto flex h-full max-w-[500px] flex-col justify-center p-8">
+        <Image
+          className="mb-8"
+          src="logo.svg"
+          width={173}
+          height={39}
+          alt="Finance Ai"
+        />
+        <h1 className="mb-3 text-4xl font-bold">Bem-vindo</h1>
+        <p className="mb-8 text-muted-foreground">
+          A Finance AI é uma plataforma de gestão financeira que utiliza IA para
+          monitorar suas movimentações, e oferecer insights personalizados,
+          facilitando o controle do seu orçamento.
+        </p>
+        <SignInButton>
+          <Button variant="outline">
+            <LogInIcon className="mr-2" />
+            Fazer login ou criar conta
+          </Button>
+        </SignInButton>
+      </div>
+      <div className="relative h-full w-full">
+        <Image
+          className="object-cover"
+          src={"/login.png"}
+          fill
+          alt="Faça login"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
